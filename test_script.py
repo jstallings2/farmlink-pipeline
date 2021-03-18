eabfa3bb0ca581a9da0dfe2c2788c699fa57e54a
stallijc <jacob.c.stallings@vanderbilt.edu>
2021-03-18 08:06:48 -0500

    Set back to pull 1 week. Will need to change from using local CSV to using Storage API to get CSV from Cloud Storage

--- test_script.py
+++ test_script.py
@@ -2,6 +2,7 @@ import os
 import requests
 import pandas as pd 
 import numpy as np 
+import json
 from datetime import datetime
 from datetime import timedelta
 from dateutil.relativedelta import relativedelta
@@ -220,34 +221,42 @@ def nearest_date(dates, targdate):
     return nearest, timedelta
 
 def adjust_inflation(data, coeffs):
-    adjusted = data.reset_index().sort_values(by='Date')
+    adjusted = data.sort_values(by='Date')
     merged_df = pd.merge_asof(adjusted, coeffs, left_on='Date', right_on='DATE')
-    merged_df["IA Avg Price"] = (merged_df['Weighted Avg Price']/merged_df['CPIAUCNS'])*100
+    # Normalize CPI with most recent CPI being 1.0
+    
+    
+    merged_df["IA Avg Price"] = (merged_df['Weighted Avg Price']/merged_df['CPIAUCNS'])
     merged_df = merged_df.set_index('Date')
     merged_df = merged_df.sort_index()
-    print('Data with inflation index added:\n', merged_df.head())
     return merged_df
 
+"""
+Load CPI Data from a local csv, clean and adjust so everything is in 2021 dollars
+"""
 def load_cpi_data():
     coeffs = pd.read_csv('./CPI_DATA.csv')
     coeffs['DATE'] = pd.to_datetime(coeffs['DATE'])
     coeffs = coeffs.sort_values(by='DATE')
     coeffs = coeffs.reset_index(drop=True)
+    most_recent = coeffs.iloc[-1]['CPIAUCNS']
+    coeffs['CPIAUCNS'] = coeffs['CPIAUCNS'].divide(most_recent)
     return coeffs
 
 
 if __name__ == "__main__":
     coeffs = load_cpi_data()
     for r in test_regions:
+        data = {}
         for v in test_producenames:
             results_df = pd.DataFrame()
             input_df = update_data(r, v, test=True)
             adjusted_df = adjust_inflation(input_df, coeffs)
-            results_df = results_df.append(calc_averages(r, v, adjusted_df,adjusted=True), ignore_index=True)
-            print(results_df)
-            print(results_df.columns)
-            path = JSON_DIR + r + '_'+ v + '.json'
-            results_df.to_json(path)
+            result = calc_averages(r, v, adjusted_df,adjusted=True)
+            data[v] = result
+        path = JSON_DIR + r + '.json'
+        with open(path, 'w') as fp:
+            json.dump(data, fp)
                 
     
 
